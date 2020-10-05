85f63a2997811673e7ea377ecb3ad28b412f9da2
Gerard Boyle <ger_boyle@yahoo.ie>
2020-10-05 10:58:23 +0100

    Test commit for dev to main pell request check

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=process.env.POSTGRESS_USERNAME
-export POSTGRES_PASSWORD=
+export POSTGRES_USERNAME=process.env.POSTGRESS_USERNAME;
+export POSTGRES_PASSWORD=process.env.POSTGRESS_PASSWORD;
 export POSTGRES_HOST=
 export POSTGRES_DB=
 export AWS_BUCKET=
