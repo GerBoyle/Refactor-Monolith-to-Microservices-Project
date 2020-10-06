ed489cfa01d3c116107c870bb10fd0271bb3682e
Gerard Boyle <ger_boyle@yahoo.ie>
2020-10-06 13:35:41 +0100

    Setting up env variables

--- set_env.sh
+++ set_env.sh
@@ -1,12 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=process.env.POSTGRESS_USERNAME;
-export POSTGRES_PASSWORD=process.env.POSTGRESS_PASSWORD;
-
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=process.env.POSTGRES_USERNAME;
+export POSTGRES_PASSWORD=process.env.POSTGRES_PASSWORD;
+export POSTGRES_HOST=process.env.POSTGRES_HOST;
+export POSTGRES_DB=process.env.POSTGRES_DB;
+export AWS_BUCKET=process.env.AWS_BUCKET;
+export AWS_REGION=process.env.AWS_REGION;
+export AWS_PROFILE=process.env.AWS_PROFILE;
+export JWT_SECRET=process.env.JWT_SECRET;
 export URL=http://localhost:8100
